c3ed1fad294430d11ebb17474072373d857a0b2b
Thiago Santos <thiago.psantos@tvglobo.com.br>
2019-06-07 23:51:16 -0300

    Updating package file

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.2"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("5.0.0-beta.3")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
